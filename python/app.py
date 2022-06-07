2e4f67dfcd81dee3805fc2354bab035ed8d64357
Anthony Salemo <ajssalemo@gmail.com>
2022-06-06 21:07:48 -0400

    adding a basic python example for testing

---
+++ python/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return jsonify({ "msg": "custom-request-header-size-python" })
+
+
+@app.route("/api/log/headers")
+def headers():
+    return jsonify({ "msg": dict(request.headers) })
